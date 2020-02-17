73ef5fbbee7e55f43b42c93195e0e698148ea1f2
AgathePlunian <agathe.plunian@gmail.com>
2020-02-17 18:41:55 +0100

    mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   validates :encrypted_password, presence: true
   has_many :participations
   has_many :events, through: :participations
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
